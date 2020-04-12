3618b271a41fcf1126c5e76cd41908b484126c5c
Ignacio Mosconi <nacho7577@hotmail.com>
2020-04-12 17:04:20 -0300

    Re-Made the Roulette Spinning Animation & Made the Resulting Color Be Highlighted

--- Assets/Scripts/Custom Components/Tool Controllers/RouletteController.cs
+++ Assets/Scripts/Custom Components/Tool Controllers/RouletteController.cs
@@ -10,23 +10,19 @@ namespace MetaMonster
         [SerializeField] Image[] pieChartImages = default;
         [SerializeField] Transform rouletteArrowContainer = default;
 
+        [Header("Other Properties")]
+        [SerializeField, Range(0f, 1f)] float nonResultingColorDim = default; 
+
+        Image resultingPieChartImage;
         bool isSpinning = false;
-
-        void Awake()
-        {
-            Color[] rouletteColors = ToolsManager.Instance.RouletteColors;
-
-            for (int i = 0; i < pieChartImages.Length; i++)
-            {
-                pieChartImages[i].color = rouletteColors[i];
-                pieChartImages[i].fillMethod = Image.FillMethod.Radial360;   
-            }
-        }
 
         void SetUpRoulette(int[] colorAmounts)
         {
             int colorSum = 0;
             float currentPieAngle = 0f;
+            float randomArrowAngle = -Random.Range(0f, 360f);
+
+            rouletteArrowContainer.rotation = Quaternion.AngleAxis(randomArrowAngle, Vector3.forward);
 
             for (int i = 0; i < colorAmounts.Length; i++)
                 colorSum += colorAmounts[i];
@@ -34,24 +30,47 @@ namespace MetaMonster
             for (int i = 0; i < pieChartImages.Length; i++)
             {
                 float piecePercentage = (float)colorAmounts[i] / colorSum;
+                float previousPieAngle = currentPieAngle;
 
                 pieChartImages[i].gameObject.SetActive(colorAmounts[i] > 0);
                 pieChartImages[i].transform.rotation = Quaternion.AngleAxis(currentPieAngle, Vector3.forward);
                 pieChartImages[i].fillAmount = piecePercentage;
 
                 currentPieAngle -= Mathf.Lerp(0f, 360f, piecePercentage);
+
+                if (randomArrowAngle < previousPieAngle && randomArrowAngle >= currentPieAngle)
+                    resultingPieChartImage = pieChartImages[i];
             }
-
-            float randomArrowAngle = Random.Range(0f, 360f);
-
-            rouletteArrowContainer.rotation = Quaternion.AngleAxis(randomArrowAngle, Vector3.forward);
         }
 
-        IEnumerator WaitForSpinEnd()
+        void HighlightResultingColor()
+        {
+            for (int i = 0; i < pieChartImages.Length; i++)
+                if (pieChartImages[i] != resultingPieChartImage)
+                {
+                    Color dimmedColor = pieChartImages[i].color * nonResultingColorDim;
+                    
+                    dimmedColor.a = 1f;
+                    pieChartImages[i].color = dimmedColor;
+                }
+        }
+
+        void ResetRouletteHighlightState()
+        {
+            Color[] rouletteColors = ToolsManager.Instance.RouletteColors;
+
+            for (int i = 0; i < pieChartImages.Length; i++)
+                pieChartImages[i].color = rouletteColors[i];
+
+            resultingPieChartImage = null;
+        }
+
+        IEnumerator EndSpinning()
         {
             yield return new WaitForSeconds(showToolDuration);
 
             isSpinning = false;
+            HighlightResultingColor();
         }
 
         public override void DismissTool()
@@ -65,12 +84,13 @@ namespace MetaMonster
             if (IsToolRunning)
                 return;
 
+            ResetRouletteHighlightState();
             SetUpRoulette(colorAmounts);
 
             isSpinning = true;
 
             ShowTool(toolID);
-            StartCoroutine(WaitForSpinEnd());
+            StartCoroutine(EndSpinning());
         }
     }
 }
